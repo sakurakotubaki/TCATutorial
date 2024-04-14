a2511ac638c43f593060bd1d05b4b5f1c7b34123
Jboy422 <gitonly543@gmail.com>
2024-04-14 18:05:10 +0900

    Initial Commit

---
+++ TCATutorial/TCATutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialApp.swift
+//  TCATutorial
+//
+//  Created by Jboy422 on 2024/04/14.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
